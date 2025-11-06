1cc93766b9419c5849e8c9c35a505479fe63a5c5
julopezlop <julopezlop@unal.edu.co>
2025-11-05 19:56:25 -0500

    Add files via upload

---
+++ pruebaSnake.py
@@ -0,0 +1,9 @@
+from ejecutador import Juego
+
+# Para jugar Tetris
+# juego_tetris = Juego("arbol_tetris.ast", "tetris")
+# juego_tetris.run_game_tetris()
+
+# Para jugar Snake
+juego_snake = Juego("arbol_snake.ast", "snake")
+juego_snake.run_game_snake()
